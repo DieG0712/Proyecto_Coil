94f0d39508c7d6f3871bdbd7ab605e9115249141
Diego <diegosilvabazan27@gmail.com>
2025-05-17 20:01:24 -0500

    microondas hecho

--- proyecto/src/hijo/Microondas.java
+++ proyecto/src/hijo/Microondas.java
@@ -1,5 +1,24 @@
 package hijo;
 
-public class Microondas {
+import padre.Electrodomesticos;
 
+public class Microondas extends Electrodomesticos{
+	
+	public Microondas(String nomProducto, double precio, int cantStok, String marca,
+			String modelo, int garantia) {
+		super(nomProducto, precio, cantStok, marca, modelo, garantia);
+	}
+
+	public String registrar() {
+		return "Producto registrado: " + nomProducto ;
+	}
+	public String mostrarInfo() {
+		return "=== Información de la Refrigeradora ===\n" +
+			       "Nombre: " + nomProducto + "\n" +
+			       "Precio: S/ " + precio + "\n" +
+			       "Stock disponible: " + cantStok + "\n" +
+			       "Marca: " + marca + "\n" +
+			       "Modelo: " + modelo + "\n" +
+			       "Garantía: " + garantia + " meses\n";
+	}	
 }
