0e69f91375d3b9d5c03f3f4ca2a36a09ef6022d1
Diego <diegosilvabazan27@gmail.com>
2025-05-17 19:59:57 -0500

    alimentos

--- proyecto/src/padre/Alimentos.java
+++ proyecto/src/padre/Alimentos.java
@@ -2,6 +2,25 @@ package padre;
 
 import abuelo.Producto;
 
-public class Alimentos {
-
+public abstract class Alimentos extends Producto{
+	protected String fechaVencimiento;
+	protected double peso;
+	public Alimentos(String nomProducto, double precio, int cantStok, String fechaVencimiento, double peso) {
+		super(nomProducto, precio, cantStok);
+		this.fechaVencimiento = fechaVencimiento;
+		this.peso = peso;
+	}
+	public String getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	public void setFechaVencimiento(String fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 }
